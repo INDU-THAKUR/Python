b02a720e0d64a54c145cb9902001f7f1c7b8b63d
INDU-THAKUR <indu82.it@gmail.com>
2023-11-04 12:41:55 +0530

    files deleted

--- Basics/datatype.py
+++
@@ -1,9 +0,0 @@
-#various data types is used in python
-#interger
-#string
-#complex
-#boolean
-#list
-#tuples
-#set
-#dictionary
--- Basics/in.py
+++
@@ -1,7 +0,0 @@
-names=["harry","subham","rohit","rohan","aditi","akshay"]
-name =input("enter the name to check\n")
-
-if(name in names):
-    print("your name is present in the list")
-else:
-    print("your name is not present in the list")
