6dd721849e8a16837fce715d26efd6b461eb208b
INDU-THAKUR <indu82.it@gmail.com>
2023-11-04 14:42:00 +0530

    added

--- Basics/readinginput.py
+++
@@ -1,3 +0,0 @@
-a=input("enter your name\n") # --> this function allws the user to take input as a string
-print("my name is",a)
-print(type(a)) 
--- Basics/typecasting.py
+++
@@ -1,6 +0,0 @@
-a="2343"
-print(type(a))
-a=float(a) # -->typecasting
-print(type(a))
-# print(a+5) --> give you an error
-print(a+6)
